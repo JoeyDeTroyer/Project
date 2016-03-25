085a5dd19a4e1ae0dffa34490db5d49961d13318
Joey De Troyer <rilextus@Joey>
2016-03-25 14:49:54 +0100

    Confirmation dialog voor knop verwijderen toegevoegd. 'Niemand geselecteerd'-exception opgelost.

--- src/gui/FXMLInlogschermController.java
+++ src/gui/FXMLInlogschermController.java
@@ -3,13 +3,13 @@ package gui;
 import DAO.LeerlingDAO;
 import Models.Leerling;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.collections.ObservableList;
+import java.util.Optional;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
@@ -51,10 +51,34 @@ public class FXMLInlogschermController extends BorderPane
     @FXML
     public void verwijder()
     {
-        Leerling l = lstGebruikers.getSelectionModel().getSelectedItem();
-        Model.getInstance().getLeerlingen().remove(l);
-        LeerlingDAO.getInstance().verwijderLeerling(l.getInschrijvingsNr());
-        initialize();
+        if(lstGebruikers.getSelectionModel().getSelectedItem() == null)
+        {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Opgelet");
+            alert.setHeaderText(null);
+            alert.setContentText("U heeft niemand geselecteerd.");
+            alert.showAndWait();
+        }
+        else
+        {
+            Leerling l = lstGebruikers.getSelectionModel().getSelectedItem();
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Leerling verwijderen");
+            alert.setHeaderText(null);
+            alert.setContentText("Wilt u " + l.getNaamVoornaam() + " verwijderen?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.get() == ButtonType.OK)
+            {
+                Model.getInstance().getLeerlingen().remove(l);
+                LeerlingDAO.getInstance().verwijderLeerling(l.getInschrijvingsNr());
+                initialize();
+            }
+            else
+            {
+
+            }
+        }
     }
 
     
